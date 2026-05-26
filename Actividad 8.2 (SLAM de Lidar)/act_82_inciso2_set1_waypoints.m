f99c8e9a5e1ccf242b1c3c2bcfc84cc2853dbf08
Marcos Allen Martínez Cortés <marcosallenmtzc@gmail.com>
2026-05-26 16:07:14 -0600

    Files added

---
+++ Actividad 8.2 (SLAM de Lidar)/act_82_inciso2_set1_waypoints.m
@@ -0,0 +1,111 @@
+%% Simulation setup
+% Define Vehicle
+R = 0.1;                        % Wheel radius [m]
+L = 0.5;                        % Wheelbase [m]
+dd = DifferentialDrive(R,L);
+
+% Sample time and time array
+sampleTime = 0.1;              % Sample time [s]
+tVec = 0:sampleTime:177;        % Time array
+
+% Initial conditions
+initPose = [6.25;19;0];            % Initial pose (x y theta)
+pose = zeros(3,numel(tVec));   % Pose matrix
+pose(:,1) = initPose;
+
+
+% Load map
+
+%complexMap       41x52                2132  logical              
+%emptyMap         26x27                 702  logical              
+%simpleMap        26x27                 702  logical              
+%ternaryMap      501x501            2008008  double  
+
+close all
+load complexMap
+
+% Create lidar sensor
+lidar = LidarSensor;
+lidar.sensorOffset = [0,0];
+lidar.scanAngles = linspace(-pi,pi,200);%51
+lidar.maxRange = 1.1;%5
+
+% Create visualizer
+viz = Visualizer2D;
+viz.hasWaypoints = true;
+viz.mapName = 'map';
+attachLidarSensor(viz,lidar);
+
+%% Path planning and following
+
+% Create waypoints
+waypoints = [initPose(1:2)';
+             8 15; %auxiliar
+             6.25 14;
+             2 14; %auxiliar
+             2 9; %auxiliar
+             6.25 9;
+             8 6.5; %auxiliar
+             8 4.5; %auxiliar
+             6.25 4;
+             10 4.5; %auxiliar
+             12.5 4;
+             13.5 5.5; %auxiliar
+             12.5 9;
+             13.5 5.5; %auxiliar
+             18.75 4;
+             25 4;
+             16 5; %auxiliar
+             18.75 9;
+             25 9;
+             25 14;
+             25 19;
+             18.75 19;
+             16 16; %auxiliar
+             18.75 14;
+             12.5 14;
+             12.5 19];
+
+% Pure Pursuit Controller
+controller = controllerPurePursuit;
+controller.Waypoints = waypoints;
+controller.LookaheadDistance = 0.35;%0.5
+controller.DesiredLinearVelocity = 0.75; %0.75
+controller.MaxAngularVelocity = 20;
+
+% Vector Field Histogram (VFH) for obstacle avoidance
+vfh = controllerVFH;
+vfh.DistanceLimits = [0.05 3]; %0.05 3
+vfh.NumAngularSectors = 36; %36
+vfh.HistogramThresholds = [5 10]; % 5y 10
+vfh.RobotRadius = L;
+vfh.SafetyDistance = L;
+vfh.MinTurningRadius = 0.1;%0.25
+
+%% Simulation loop
+r = rateControl(1/sampleTime);
+for idx = 2:numel(tVec) 
+    
+    % Get the sensor readings
+    curPose = pose(:,idx-1);
+    ranges = lidar(curPose);
+        
+    % Run the path following and obstacle avoidance algorithms
+    [vRef,wRef,lookAheadPt] = controller(curPose);
+    targetDir = atan2(lookAheadPt(2)-curPose(2),lookAheadPt(1)-curPose(1)) - curPose(3);
+    steerDir = vfh(ranges,lidar.scanAngles,targetDir);    
+    if ~isnan(steerDir) && abs(steerDir-targetDir) > 0.1
+        wRef = 0.5*steerDir;
+    end
+    
+    % Control the robot
+    velB = [vRef;0;wRef];                   % Body velocities [vx;vy;w]
+    vel = bodyToWorld(velB,curPose);  % Convert from body to world
+    
+    % Perform forward discrete integration step
+    pose(:,idx) = curPose + vel*sampleTime; 
+    
+    % Update visualization
+    viz(pose(:,idx),waypoints,ranges)
+    waitfor(r);
+end
---
+++ Actividad 8.2 (SLAM de Lidar)/act_82_inciso2_set2_waypoints.m
@@ -0,0 +1,100 @@
+%% Simulation setup
+% Define Vehicle
+R = 0.1;                        % Wheel radius [m]
+L = 0.5;                        % Wheelbase [m]
+dd = DifferentialDrive(R,L);
+
+% Sample time and time array
+sampleTime = 0.1;              % Sample time [s]
+tVec = 0:sampleTime:111;        % Time array
+
+% Initial conditions
+initPose = [22;17;pi];            % Initial pose (x y theta)
+pose = zeros(3,numel(tVec));   % Pose matrix
+pose(:,1) = initPose;
+
+
+% Load map
+
+%complexMap       41x52                2132  logical              
+%emptyMap         26x27                 702  logical              
+%simpleMap        26x27                 702  logical              
+%ternaryMap      501x501            2008008  double  
+
+close all
+load complexMap
+
+% Create lidar sensor
+lidar = LidarSensor;
+lidar.sensorOffset = [0,0];
+lidar.scanAngles = linspace(-pi,pi,200);%51
+lidar.maxRange = 1;%5
+
+% Create visualizer
+viz = Visualizer2D;
+viz.hasWaypoints = true;
+viz.mapName = 'map';
+attachLidarSensor(viz,lidar);
+
+%% Path planning and following
+
+% Create waypoints
+waypoints = [initPose(1:2)';
+             19.5 19; %auxiliar
+             16 16; %auxiliar
+             9 14.5; %auxiliar
+             8 15.5; %auxiliar
+             3 18;
+             8 15; %auxiliar
+             3 14; %auxiliar
+             1.5 9; %auxiliar
+             8 6.5; %auxiliar
+             8 1.5; %auxiliar
+             7 2;
+             9 4.5; %auxiliar
+             24 4.5 %auxiliar
+             24 1];
+
+% Pure Pursuit Controller
+controller = controllerPurePursuit;
+controller.Waypoints = waypoints;
+controller.LookaheadDistance = 0.35;%0.5
+controller.DesiredLinearVelocity = 0.75; %0.75
+controller.MaxAngularVelocity = 20;
+
+% Vector Field Histogram (VFH) for obstacle avoidance
+vfh = controllerVFH;
+vfh.DistanceLimits = [0.05 3]; %0.05 3
+vfh.NumAngularSectors = 36; %36
+vfh.HistogramThresholds = [5 10]; % 5y 10
+vfh.RobotRadius = L;
+vfh.SafetyDistance = L;
+vfh.MinTurningRadius = 0.1;%0.25
+
+%% Simulation loop
+r = rateControl(1/sampleTime);
+for idx = 2:numel(tVec) 
+    
+    % Get the sensor readings
+    curPose = pose(:,idx-1);
+    ranges = lidar(curPose);
+        
+    % Run the path following and obstacle avoidance algorithms
+    [vRef,wRef,lookAheadPt] = controller(curPose);
+    targetDir = atan2(lookAheadPt(2)-curPose(2),lookAheadPt(1)-curPose(1)) - curPose(3);
+    steerDir = vfh(ranges,lidar.scanAngles,targetDir);    
+    if ~isnan(steerDir) && abs(steerDir-targetDir) > 0.1
+        wRef = 0.5*steerDir;
+    end
+    
+    % Control the robot
+    velB = [vRef;0;wRef];                   % Body velocities [vx;vy;w]
+    vel = bodyToWorld(velB,curPose);  % Convert from body to world
+    
+    % Perform forward discrete integration step
+    pose(:,idx) = curPose + vel*sampleTime; 
+    
+    % Update visualization
+    viz(pose(:,idx),waypoints,ranges)
+    waitfor(r);
+end
